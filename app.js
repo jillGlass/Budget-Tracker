f298bb5408cfb1aa43db6216350f0d5fe9247684
JillGlass <glass.nz@gmail.com>
2020-05-27 21:33:47 +1200

    add keypress and click event for entering item

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var budgetController = (function() {
- 
-})();
+var budgetController = (function() {})();
 
 var UIController = (function() {
   //write code later
 })();
 
-//controller to control UIController and budgetController functions
+//global app controller to control UIController and budgetController functions
 var controller = (function(budgetCtrl, UICtrl) {
-  
+  var ctrlAddItem = function() {
+    //1. Get the filed input data
+    //2. Add the item to the budget controller
+    //3. Add the item to the user interface
+    //4. Calculate budget
+    //5. Display budget on user interface
+    console.log('it works on enter')
+  };
 
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem()
+    }
+  });
 })(budgetController, UIController);
