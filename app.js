55a2782c4842955b58b4ed56b9ef818e380e6ccf
JillGlass <glass.nz@gmail.com>
2020-05-27 21:04:11 +1200

    module structure defined

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var budgetController = (function() {
+ 
+})();
 
+var UIController = (function() {
+  //write code later
+})();
+
+//controller to control UIController and budgetController functions
+var controller = (function(budgetCtrl, UICtrl) {
+  
+
+
+})(budgetController, UIController);
