b2ca66913731e14c9a1eca6e604efd6614d5609d
JillGlass <glass.nz@gmail.com>
2020-05-27 21:46:27 +1200

    get input function created and logging to console

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var budgetController = (function() {})();
 
 var UIController = (function() {
-  //write code later
+  
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector('.add__type').value, //will be inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            }
+           
+        }
+    }
 })();
 
 //global app controller to control UIController and budgetController functions
 var controller = (function(budgetCtrl, UICtrl) {
   var ctrlAddItem = function() {
     //1. Get the filed input data
+    var input = UICtrl.getInput()
+    console.log(input)
     //2. Add the item to the budget controller
+
     //3. Add the item to the user interface
+
     //4. Calculate budget
+
     //5. Display budget on user interface
+    
     console.log('it works on enter')
   };
 
